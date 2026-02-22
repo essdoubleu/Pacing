2421a58676b0773507dd44096bc91e41840c5d4d
Scott <scottwindrem@gmail.com>
2026-02-21 23:36:43 -0500

    Update subscribe.js
    line 9 changed to module.exports = async function handler(req, res) {

--- api/subscribe.js
+++ api/subscribe.js
@@ -6,7 +6,7 @@ const { Resend } = require("resend");
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Only allow POST
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
